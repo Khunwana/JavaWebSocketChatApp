376a35e6fd86a8f4d430762059e8235442fbd9f9
Monias <khunwana.monias@gmail.com>
2025-03-26 22:31:48 +0200

    WebsocketConfig commit
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/khunwana/websocket_app/WebsocketConfig.java
@@ -0,0 +1,10 @@
+package com.khunwana.websocket_app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig {
+
+}
