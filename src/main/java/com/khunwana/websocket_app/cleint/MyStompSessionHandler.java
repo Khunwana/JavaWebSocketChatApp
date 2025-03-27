c254e07262219b0177ba41f235c7e34c2054ed37
Monias <khunwana.monias@gmail.com>
2025-03-28 00:42:13 +0200

    Handling messages thatare being sent from client
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/khunwana/websocket_app/cleint/MyStompSessionHandler.java
@@ -0,0 +1,56 @@
+package com.khunwana.websocket_app.cleint;
+
+import java.lang.reflect.Type;
+
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+
+import com.khunwana.websocket_app.Message;
+
+public class MyStompSessionHandler extends StompSessionHandlerAdapter{
+	
+	private String username;
+	
+	
+	
+	public MyStompSessionHandler(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+		session.subscribe("/topic/messages", new StompFrameHandler() {
+
+			@Override
+			public Type getPayloadType(StompHeaders headers) {
+				// TODO Auto-generated method stub
+				return Message.class;
+			}
+
+			@Override
+			public void handleFrame(StompHeaders headers, Object payload) {
+				try {
+					if(payload instanceof Message)
+					{
+						Message message = (Message) payload;
+						System.out.println("Recieved message: "+message.getUser()+": "+message.getMessage());
+					}
+					else
+					{
+						System.out.println("Recieved unexpected payload type: "+ payload.getClass());
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	@Override
+	public void handleTransportError(StompSession session, Throwable exception) {
+	}
+
+
+}
